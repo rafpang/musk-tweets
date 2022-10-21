f599f509bcc36231f02a8e23b11a24fbbb38223c
rafpang <rafibintangpagi@gmail.com>
2022-10-21 16:01:43 +0800

    added sentiments to tweets

--- add_sentiments.py
+++ add_sentiments.py
@@ -1,26 +1,14 @@
+
+import pandas as pd
+import numpy as np
+
+
 from transformers import AutoModelForSequenceClassification
 from transformers import TFAutoModelForSequenceClassification
-from transformers import AutoTokenizer
-
-
+from transformers import AutoTokenizer, AutoConfig
 import numpy as np
 from scipy.special import softmax
-import csv
-import urllib.request
-import pandas as pd
-
-
-
-labels=[]
-mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/sentiment/mapping.txt"
-with urllib.request.urlopen(mapping_link) as f:
-    html = f.read().decode('utf-8').split("\n")
-    csvreader = csv.reader(html, delimiter='\t')
-labels = [row[1] for row in csvreader if len(row) > 1]
-
-MODEL = "cardiffnlp/twitter-roberta-base-sentiment"
-tokenizer = AutoTokenizer.from_pretrained(MODEL)
-
+# Preprocess text (username and link placeholders)
 def preprocess(text):
     new_text = []
     for t in text.split(" "):
@@ -28,22 +16,23 @@ def preprocess(text):
         t = 'http' if t.startswith('http') else t
         new_text.append(t)
     return " ".join(new_text)
+MODEL = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+config = AutoConfig.from_pretrained(MODEL)
 
 model = AutoModelForSequenceClassification.from_pretrained(MODEL)
-model.save_pretrained(MODEL)
+
 
 if __name__ == '__main__':
 
     df = pd.read_csv('ElonTweets.csv')
     #tweets_and_sentment 
     tweets = df.Text.copy()
-    
 
     preprocessed_tweets = []
     for i, row in tweets.iterrows():
         text = row.Text
         text = preprocess(text)
-        preprocessed_tweets.append(text)
         encoded_input = tokenizer(text, return_tensors='pt')
         output = model(**encoded_input)
         scores = output[0][0].detach().numpy()
@@ -51,9 +40,7 @@ if __name__ == '__main__':
         ranking = np.argsort(scores)
         ranking = ranking[::-1]
         for i in range(scores.shape[0]):
-            l = labels[ranking[i]]
+            l = config.id2label[ranking[i]]
             s = scores[ranking[i]]
-            output_text = f"{i+1}) {l} {np.round(float(s), 4)}"
-
-    #returns the order
+            print(f"{i+1}) {l} {np.round(float(s), 4)}")
 
