d4c6dfc90e87f316d93c4424994f4425ed933d70
rafpang <rafibintangpagi@gmail.com>
2022-10-21 15:53:57 +0800

    added py files for sentiment

--- add_sentiments.py
+++ add_sentiments.py
@@ -1,15 +1,24 @@
-
-import numpy as np
-import pandas as pd
-from transformers import pipeline
 from transformers import AutoModelForSequenceClassification
 from transformers import TFAutoModelForSequenceClassification
 from transformers import AutoTokenizer
+
+
 import numpy as np
 from scipy.special import softmax
+import csv
+import urllib.request
+import pandas as pd
 
 
-MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
+
+labels=[]
+mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/sentiment/mapping.txt"
+with urllib.request.urlopen(mapping_link) as f:
+    html = f.read().decode('utf-8').split("\n")
+    csvreader = csv.reader(html, delimiter='\t')
+labels = [row[1] for row in csvreader if len(row) > 1]
+
+MODEL = "cardiffnlp/twitter-roberta-base-sentiment"
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
 
 def preprocess(text):
@@ -20,10 +29,31 @@ def preprocess(text):
         new_text.append(t)
     return " ".join(new_text)
 
-
+model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+model.save_pretrained(MODEL)
 
 if __name__ == '__main__':
 
     df = pd.read_csv('ElonTweets.csv')
-    preprocessed_df = df.Text.apply(preprocess)
-    print(preprocessed_df.head())
+    #tweets_and_sentment 
+    tweets = df.Text.copy()
+    
+
+    preprocessed_tweets = []
+    for i, row in tweets.iterrows():
+        text = row.Text
+        text = preprocess(text)
+        preprocessed_tweets.append(text)
+        encoded_input = tokenizer(text, return_tensors='pt')
+        output = model(**encoded_input)
+        scores = output[0][0].detach().numpy()
+        scores = softmax(scores)
+        ranking = np.argsort(scores)
+        ranking = ranking[::-1]
+        for i in range(scores.shape[0]):
+            l = labels[ranking[i]]
+            s = scores[ranking[i]]
+            output_text = f"{i+1}) {l} {np.round(float(s), 4)}"
+
+    #returns the order
+
