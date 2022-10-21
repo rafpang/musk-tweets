8ed24ac7178e3183d58b883935b79e0411ac7e3f
rafpang <rafibintangpagi@gmail.com>
2022-10-21 10:30:46 +0800

    added preprocessing

--- add_sentiments.py
+++ add_sentiments.py
@@ -1,16 +1,29 @@
-import torch
-import torch.nn as nn
+
 import numpy as np
 import pandas as pd
-
 from transformers import pipeline
+from transformers import AutoModelForSequenceClassification
+from transformers import TFAutoModelForSequenceClassification
+from transformers import AutoTokenizer
+import numpy as np
+from scipy.special import softmax
 
 
+MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+
+def preprocess(text):
+    new_text = []
+    for t in text.split(" "):
+        t = '@user' if t.startswith('@') and len(t) > 1 else t
+        t = 'http' if t.startswith('http') else t
+        new_text.append(t)
+    return " ".join(new_text)
 
 
 
 if __name__ == '__main__':
+
     df = pd.read_csv('ElonTweets.csv')
-    pipe = pipeline(model="roberta-large-mnli")
-    print(pipe(df.iloc[1].Text))
-    print(df.iloc[1].Text)
+    preprocessed_df = df.Text.apply(preprocess)
+    print(preprocessed_df.head())
