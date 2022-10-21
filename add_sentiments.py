091a70f8e48c8876362246c9eadaeb9abe90cb1d
rafpang <rafibintangpagi@gmail.com>
2022-10-21 16:25:21 +0800

    finalized add sentiments

--- add_sentiments.py
+++ add_sentiments.py
@@ -1,14 +1,11 @@
 
 import pandas as pd
 import numpy as np
-
-
 from transformers import AutoModelForSequenceClassification
 from transformers import TFAutoModelForSequenceClassification
 from transformers import AutoTokenizer, AutoConfig
-import numpy as np
 from scipy.special import softmax
-# Preprocess text (username and link placeholders)
+
 def preprocess(text):
     new_text = []
     for t in text.split(" "):
@@ -27,20 +24,32 @@ if __name__ == '__main__':
 
     df = pd.read_csv('ElonTweets.csv')
     #tweets_and_sentment 
-    tweets = df.Text.copy()
-
+    tweets = df[['Text']].copy()
+    sentiments = []
+    sentiment_scores = []
     preprocessed_tweets = []
     for i, row in tweets.iterrows():
         text = row.Text
         text = preprocess(text)
+        preprocessed_tweets.append(text) 
         encoded_input = tokenizer(text, return_tensors='pt')
         output = model(**encoded_input)
         scores = output[0][0].detach().numpy()
         scores = softmax(scores)
         ranking = np.argsort(scores)
         ranking = ranking[::-1]
+        scores_list = []
+
         for i in range(scores.shape[0]):
             l = config.id2label[ranking[i]]
             s = scores[ranking[i]]
-            print(f"{i+1}) {l} {np.round(float(s), 4)}")
-
+            label_score = (l,s)
+            scores_list.append(label_score)
+        
+        sentiment, sentiment_score = scores_list[0][0], scores_list[0][1]
+        sentiments.append(sentiment)
+        sentiment_scores.append(sentiment_score)
+    df['sentiment'] = sentiments
+    df['sentiment_score'] = sentiment_scores
+    df['preprocessed_tweet'] = preprocessed_tweets
+    df.to_csv('final_data.csv')
