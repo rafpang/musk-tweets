d51bab8ca522ba6e9bb1028745a926ee270f5e58
rafpang <rafibintangpagi@gmail.com>
2022-10-24 16:55:57 +0800

    updated main.py

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ from transformers import AdamW
 from transformers import get_linear_schedule_with_warmup
 
 
-def run(df_train, ):
+def main(df_train, ):
     dfx = pd.read_csv(config.TRAINING_FILE).fillna("none")
     dfx.sentiment = dfx.sentiment.apply(lambda x: 1 if x == "positive" else 0)
 
     df_train, df_valid = model_selection.train_test_split(
-        dfx, test_size=0.1, random_state=42, stratify=dfx.sentiment.values
+        dfx, test_size=0.1, random_state=42, 
     )
 
     df_train = df_train.reset_index(drop=True)
