1ea4da9ea0cd0647057c4d4abf5d0c2bf861a3c7
Rafi Pangestu <49834348+rafpang@users.noreply.github.com>
2022-11-02 03:30:03 +0800

    Create main_rtw.py

---
+++ main_rtw.py
@@ -0,0 +1,80 @@
+import config
+import dataset
+import engine
+import torch
+import pandas as pd
+import torch.nn as nn
+import numpy as np
+
+from model import BERTBaseUncased
+from sklearn import model_selection
+from sklearn import metrics
+from transformers import AdamW
+from transformers import get_linear_schedule_with_warmup
+
+
+def main_like():
+    final_data = pd.read_csv('final_data.csv')
+
+    df_train, df_valid = model_selection.train_test_split(
+        final_data, test_size=0.1, random_state=42, 
+    )
+
+    df_train = df_train.reset_index(drop=True)
+    df_valid = df_valid.reset_index(drop=True)
+
+    train_dataset = dataset.BERTDataset(
+        tweet=df_train['preprocessed_tweet'].values, target=df_train['like count'].values
+    )
+
+    train_data_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=config.TRAIN_BATCH_SIZE, num_workers=4
+    )
+
+    valid_dataset = dataset.BERTDataset(
+        tweet=df_valid['preprocessed_tweet'].values, target=df_train['like count'].values
+    )
+
+    valid_data_loader = torch.utils.data.DataLoader(
+        valid_dataset, batch_size=config.VALID_BATCH_SIZE, num_workers=1
+    )
+
+    device = torch.device(config.DEVICE)
+    model = BERTBaseUncased()
+    model.to(device)
+
+    param_optimizer = list(model.named_parameters())
+    no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
+    optimizer_parameters = [
+        {
+            "params": [
+                p for n, p in param_optimizer if not any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": 0.001,
+        },
+        {
+            "params": [
+                p for n, p in param_optimizer if any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": 0.0,
+        },
+    ]
+
+    num_train_steps = int(len(df_train) / config.TRAIN_BATCH_SIZE * config.EPOCHS)
+    optimizer = AdamW(optimizer_parameters, lr=3e-5)
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer, num_warmup_steps=0, num_training_steps=num_train_steps
+    )
+
+    best_mse = 1e6
+    for epoch in range(config.EPOCHS):
+        engine.train_fn(train_data_loader, model, optimizer, device, scheduler)
+        outputs, targets = engine.eval_fn(valid_data_loader, model, device)
+        mse_loss = engine.loss_fn(outputs,targets)
+        print(f"Epoch {epoch} MSE Score = {mse_loss}")    
+        if mse_loss < best_mse:
+           best_mse = mse_loss
+    print(f"Best MSE obtained {best_mse}")
+    
+if __name__ == "__main__":
+    main_like()
